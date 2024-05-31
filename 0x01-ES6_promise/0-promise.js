55fbd3127071f6d3e675b5f0bec30b95d24341a1
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-05-31 13:36:48 +0000

    Task 0: Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,6 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-
+    // eslint-disable-next-line no-constant-condition
     if (true) {
       resolve();
     } else {
